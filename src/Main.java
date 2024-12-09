a8657f5970cdfd8f2ac0a088226aecd08a613021
Andrey Kononenko <anaruw_13@mail.ru>
2024-12-09 20:31:50 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 13676;
+        short bonusCost = 20;
+
+        int mileBonus = price / bonusCost;
+
+        System.out.println(mileBonus + " миль(и/я)");
+    }
+}
